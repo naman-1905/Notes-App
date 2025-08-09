a2080995514634b7b85ad4ec18a893ea945498fa
naman-1905 <namanoj19@gmail.com>
2025-08-09 14:42:03 +0530

    Updated the Notes Section

--- app/components/NotesSection.js
+++ app/components/NotesSection.js
@@ -4,7 +4,9 @@ function NotesSection() {
   return (
    <div className="font-sans">
       <h1 className="text-4xl font-bold text-center px-4 py-4">Welcome to My Notes App</h1>
-      Notes Section
+      <div>
+        
+      </div>
     </div>
   )
 }
