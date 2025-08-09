8ba0a3fb9c7cf5f1f2173e1a369a52eb890d3edf
naman-1905 <namanoj19@gmail.com>
2025-08-09 14:40:50 +0530

    Created a Notes Section

---
+++ app/components/NotesSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotesSection() {
+  return (
+   <div className="font-sans">
+      <h1 className="text-4xl font-bold text-center px-4 py-4">Welcome to My Notes App</h1>
+      Notes Section
+    </div>
+  )
+}
+
+export default NotesSection
