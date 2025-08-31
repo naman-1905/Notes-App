c84a25230e6f1320418b003533a9a3bbb13e19d8
naman-1905 <namanoj19@gmail.com>
2025-08-31 17:00:10 +0530

    Added Search bar

--- frontend/notes-app/app/(auth)/dashboard/page.jsx
+++ frontend/notes-app/app/(auth)/dashboard/page.jsx
@@ -1,12 +1,20 @@
 "use client"
 import React from 'react'
+import { useState } from 'react'
 import ProfileInfo from '@/app/components/Cards/ProfileInfo'
 import Navbar from '@/app/components/Navbar'
+import SearchBar from '@/app/components/SearchBar/SearchBar'
 
 function Homepage() {
+const [searchTerm, setSearchTerm] = useState('')
+ 
   return (
     <div>
       <ProfileInfo name="Jane Smith" onLogout={() => console.log("Logged out")} />
+        <SearchBar
+        value={searchTerm}         // controlled input
+        onChange={setSearchTerm}   // pass state updater
+      />
     </div>
   )
 }
