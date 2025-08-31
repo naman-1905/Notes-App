49edb16c4d3e9f70a3d1ee72cf2222506f3cb196
naman-1905 <namanoj19@gmail.com>
2025-08-31 13:11:19 +0530

    Added the Profile Info to the Dashboard

--- frontend/notes-app/app/(auth)/dashboard/page.jsx
+++ frontend/notes-app/app/(auth)/dashboard/page.jsx
@@ -1,9 +1,12 @@
+"use client"
 import React from 'react'
+import ProfileInfo from '@/app/components/Cards/ProfileInfo'
+import Navbar from '@/app/components/Navbar'
 
 function Homepage() {
   return (
     <div>
-      
+      <ProfileInfo name="Jane Smith" onLogout={() => console.log("Logged out")} />
     </div>
   )
 }
