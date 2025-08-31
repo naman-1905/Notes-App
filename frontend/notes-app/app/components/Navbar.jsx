6f7b2b7a88febac4385a3531443670fc50e30bfd
naman-1905 <namanoj19@gmail.com>
2025-08-31 11:45:16 +0530

    Created a Navbar component

---
+++ frontend/notes-app/app/components/Navbar.jsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+        <h2 className="text-xl font-medium text-black py-2">
+        Notes by Half Skirmish
+        </h2>
+    </div>
+  )
+}
+
+export default Navbar
