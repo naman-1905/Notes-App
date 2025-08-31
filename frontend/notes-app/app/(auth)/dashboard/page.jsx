9b3bbc75a0a694badea043cde01b66fbfe3ed554
naman-1905 <namanoj19@gmail.com>
2025-08-31 21:37:11 +0530

    Finalized the Frontend layout

--- frontend/notes-app/app/(auth)/dashboard/page.jsx
+++ frontend/notes-app/app/(auth)/dashboard/page.jsx
@@ -56,11 +56,18 @@ const Homepage = () => {
       contentLabel=''
       className='w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll'
     >
-      <AddEditNotes
-        onClose={() => {
-          setOpenAddEditModal({ isShown: false, type: "add", data: null });
-        }}
-      />
+     <AddEditNotes
+      type={openAddEditModal.type}
+      noteData={openAddEditModal.data}
+      onClose={() => setOpenAddEditModal({ isShown: false, type: "add", data: null })}
+      onSubmit={(note, actionType) => {
+        if (actionType === "edit") {
+          // update existing note
+        } else {
+          // add new note
+        }
+      }}
+    />
     </ReactModal>
     </>
   )
