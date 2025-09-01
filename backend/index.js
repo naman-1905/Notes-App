bc6e953b3b53a5210e3077ce0c2bde0c4c3f44e7
naman-1905 <namanoj19@gmail.com>
2025-09-01 18:08:15 +0530

    Created an Edit API for editing notes, tested it with Postman, Working.

--- backend/index.js
+++ backend/index.js
@@ -170,6 +170,46 @@ app.post("/add-note", authenticateToken, async(req,res) =>{
 });
 
 
+// Edit Note
+app.put("/edit-note/:noteId", authenticateToken, async(req,res) =>{
+    const noteId = req.params.noteId;
+    const {title, content, tags, isPinned} = req.body;
+    const {user} = req.user;
+
+    if(!title && !content && !tags ){
+    return res
+    .status(400)
+    .json({error: true, message: "No changes provided" });
+    }
+    
+    try{
+    const note = await Note.findOne({_id: noteId, userId:user._id});
+
+    if (!note){
+        return res.status(404).json({error:true, message: "Note not found"});
+    }
+
+    if (title) note.title = title;
+    if (content) note.content = content;
+    if (tags) note.tags = tags;
+    if (isPinned !== undefined) note.isPinned = isPinned;
+
+    await note.save();
+
+    return res.json({
+        error: false,
+        note,
+        message: "Note updated successfully",
+    });
+    } catch (error){
+        console.error("Edit note error:", error);
+        return res.status(500).json({
+            error: true,
+            message: "Interval Server Error",
+        });
+    }   
+})
+
 
 app.listen(5000);
 
