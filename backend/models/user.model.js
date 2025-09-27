f6676eb35a4bf0d1831220e8163706f85b7c809f
naman-1905 <namanoj19@gmail.com>
2025-09-27 16:09:40 +0000

    Updated the user model to store user info under collection named notes_users

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -8,4 +8,4 @@ const userSchema = new Schema({
     createdOn: {type: Date, default: new Date().getTime() },
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema, "notes_users");
